f72b92076e79a517a184fee6564d6d35e26707c2
Laurent Ngeth <l.ngeth@yahoo.fr>
2024-10-08 16:46:56 +0200

    correct scrapping

--- Scrapper.py
+++ Scrapper.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By #Permet d'accéder aux différents élements de la page web
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 
@@ -17,6 +19,7 @@ class Scrapper():
   
   def get_fire_departure_in_france_data(self, start_date, end_date):
     d = self.driver
+    d.maximize_window()
     d.get(FIRE_DEPARTURE_URL)
     assert "BDIFF : Recherche et consultation des incendies de forêt" in d.title
     time.sleep(2)
@@ -26,16 +29,19 @@ class Scrapper():
     start_date_input.clear()
     start_date_input.send_keys("value", start_date)
     assert start_date == start_date_input.get_attribute('value')
+    time.sleep(2)
 
     end_date_input = d.find_element(By.ID, 'if_dateAlerteFin_date')
     end_date_input.clear()
     end_date_input.send_keys("value", end_date)
     assert end_date == end_date_input.get_attribute('value')
+    time.sleep(2)
 
     # Click on filter button
     filter_button = d.find_element(By.ID, "if_submit")
+    d.execute_script("arguments[0].scrollIntoView(true);", filter_button)
+    time.sleep(2)
     filter_button.click()
-
     
     # Retrieve all data from table of each pages
     all_data = []
@@ -47,16 +53,22 @@ class Scrapper():
       try:
         # if disabled, stop scrapping
         next_button_li = d.find_element(By.XPATH, "//li[@class='page-item disabled']/a[@rel='next']")
+        print("bouton next désactivé")
         break
       except:
+        print("bouton next activé")
         pass
       
       # Click on next link
       try:
         next_button = d.find_element(By.CSS_SELECTOR, 'a.page-link[rel="next"]')
+        d.execute_script("arguments[0].scrollIntoView(true);", next_button)
+        time.sleep(1)
         next_button.click()
+        print("page suivante")
         time.sleep(2)
       except Exception as e:
+        print("j'ai pas réussi à appuyer sur next button")
         break
     
     d.close()
